bbd0244a363977744cf4954d024843f40bdb58e3
AbdullahSevin <SharizNative@gmail.com>
2025-01-26 12:28:27 +0300

    space_shooter_prototype

--- Pgz1.py
+++ Pgz1.py
@@ -16,7 +16,7 @@ class Player:
     def move(self, dx, dy):
         # diagonal move speed fix
         if dx != 0 and dy != 0:
-            dx *= 0.7071  # 1 / sqrt(2)
+            dx *= 0.7071
             dy *= 0.7071
 
         
@@ -25,6 +25,8 @@ class Player:
         self.x = max(0, min(WIDTH - self.size, self.x))
         self.y = max(0, min(HEIGHT - self.size, self.y))
 
+
+obstacle_spawn_point_x_randomizer = random.randint(0,WIDTH)
 
 class Obstacle1:
     def __init__(self, x, y, size, color, speed):
@@ -40,21 +42,51 @@ class Obstacle1:
     def draw(self):
         screen.draw.filled_rect(Rect((self.x, self.y), (self.size, self.size)), self.color)
         
+    def move(self):
+        self.y += self.speed
+        # Respawn it when it goes below screen
+        if self.y > HEIGHT:
+            self.respawn()
+
+    def respawn(self):
+        self.x = random.randint(0, WIDTH - self.size)
+        self.y = -self.size
+        self.speed = random.uniform(1, 5)
+
+obstacle1 = Obstacle1(
+    x=random.randint(0, WIDTH - 50),
+    y=-50,
+    size=50,
+    color=(255, 0, 0),
+    speed=random.uniform(3, 5),
+)
+obstacle2 = Obstacle1(
+    x=random.randint(0, WIDTH - 50),
+    y=-50,
+    size=50,
+    color=(255, 0, 0),
+    speed=random.uniform(3, 5),
+)
+obstacle3 = Obstacle1(
+    x=random.randint(0, WIDTH - 50),
+    y=-50,
+    size=50,
+    color=(255, 0, 0),
+    speed=random.uniform(3, 5),
+)
+
         
-obstacle_spawn_point_x_randomizer = random.range(0,WIDTH)
-obstacle_spawn_point_y_randomizer = random.range(0,HEIGHT)
+player = Player(WIDTH // 2, HEIGHT // 2, size=50, color=(0, 255, 0), speed=5) 
 
-obstacle_spawn_point_1 = WIDTH, HEIGHT
-player = Player(WIDTH // 2, HEIGHT // 2, size=50, color=(0, 255, 0), speed=5)  
-
-obstacle1 = Obstacle1(obstacle_spawn_point_x_randomizer, obstacle_spawn_point_y_randomizer, size=40, color=(255, 0, 0), speed=5)
-
-BACKGROUND_COLOR = (0, 255, 0) #green?
+BACKGROUND_COLOR = (0, 0, 255) #BLUE
 
 
 def draw():
     screen.fill(BACKGROUND_COLOR)
     player.draw()
+    obstacle1.draw()
+    obstacle2.draw()
+    obstacle3.draw()
 
 
 def update():
@@ -70,4 +102,10 @@ def update():
         dy += 1
 
     player.move(dx, dy)
-    obstacle1.move(-1)
+    obstacle1.move()
+    obstacle2.move()
+    obstacle3.move()
+
+# add collisions
+# add shooting
+# add levels
